727c1ebb2337c33800ed5ad0d868cd8c74587aed
johan52824 <merickjohan@gmail.com>
2022-01-13 12:18:08 -0500

    erickmera

---
+++ BarUleam/src/app/models/pedido.ts
@@ -0,0 +1,5 @@
+export interface pedido{
+    id?: number;
+    _usuario?: string;
+    total?:number;
+}
---
+++ BarUleam/src/app/models/usuario.ts
@@ -0,0 +1,8 @@
+export interface usuario{
+    id?: string;
+    nombre?: string;
+    cedula?: string;
+    email?: string;
+    telefono?: number;
+    password?: string;
+}
