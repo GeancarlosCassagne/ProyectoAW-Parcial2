19bbbf67657aff61535e515e4727b47f3bf94d0a
GeancarlosCassagne <geancarloscassagne15@gmail.com>
2022-01-12 21:01:47 -0500

    GeancarlosCassagne

--- panel/src/services/index.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    CategoriaService: require('./categoria.service'),
-    ProductoService: require('./producto.service'),
-    EmpleadoService: require('./empleado.service'),
-    PedidoService: require('./pedido.service'),
-}
